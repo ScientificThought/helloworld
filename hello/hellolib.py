0260ec49ed03b840fc3ddbafb1f9b649c12edff3
Marco Barbosa <aureliobarbosa@gmail.com>
2024-04-28 17:44:19 -0300

    add more functions to hellolib

--- hello/hellolib.py
+++ hello/hellolib.py
@@ -1,2 +1,14 @@
+standard_subject = "World"
+
+
+def get_hello_message(subject=standard_subject):
+    return f"Hello {subject}!"
+
+
+def hello_subject(subject=standard_subject):
+    msg = get_hello_message(subject=subject)
+    print(msg)
+
+
 def hello_world():
-    print("Hello World!")
+    hello_subject()
